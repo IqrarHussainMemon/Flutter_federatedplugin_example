cbb420756b2c826e0b1d3753e6dd9011dec93ba4
IqrarHussainMemon <imemon760@gmail.com>
2021-09-23 15:16:02 +0500

    Same code when defined in packages gives error path_provider package while builiding

--- packages/filecreation/lib/filecreation.dart
+++ packages/filecreation/lib/filecreation.dart
@@ -1,23 +1,23 @@
 library filecreation;
 
-// import 'package:path_provider/path_provider.dart';
-// import 'dart:async';
-// import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
 
-// Future<String> get _localPath async {
-//   final directory = (await getApplicationDocumentsDirectory());
-//   print(directory.path);
-//   return directory.path;
-// }
+Future<String> get _localPath async {
+  final directory = (await getApplicationDocumentsDirectory());
+  print(directory.path);
+  return directory.path;
+}
 
-// Future<File> get _localFile async {
-//   final path = await _localPath;
-//   return File('$path/sample.txt');
-// }
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/sample.txt');
+}
 
-// Future<File> writeSampleFile(String counter) async {
-//   final file = await _localFile;
+Future<File> writeSampleFile(String counter) async {
+  final file = await _localFile;
 
-//   // Write the file
-//   return file.writeAsString('$counter');
-// }
+  // Write the file
+  return file.writeAsString('$counter');
+}
