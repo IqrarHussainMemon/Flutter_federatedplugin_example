30e20ceaccd05631b889f6b6f3eac75a1ad6d991
IqrarHussainMemon <imemon760@gmail.com>
2021-09-23 15:03:46 +0500

    some files was missing in previous commit thats why I am commiting again

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:filecreation/filecreation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -50,6 +53,24 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     writeSampleFile("testing");
     //print(str);
+  }
+
+  Future<String> get _localPath async {
+    final directory = (await getApplicationDocumentsDirectory());
+    //print(directory.path);
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/sample.txt');
+  }
+
+  Future<File> writeSampleFile(String counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
   }
 
   @override
--- packages/filecreation/lib/filecreation.dart
+++ packages/filecreation/lib/filecreation.dart
@@ -1,8 +1,8 @@
 library filecreation;
 
 // import 'package:path_provider/path_provider.dart';
-import 'dart:async';
-import 'dart:io';
+// import 'dart:async';
+// import 'dart:io';
 
 // Future<String> get _localPath async {
 //   final directory = (await getApplicationDocumentsDirectory());
@@ -10,15 +10,14 @@ import 'dart:io';
 //   return directory.path;
 // }
 
-Future<File> get _localFile async {
-  //final path = await _localPath;
-  return File(
-      '/Users/iqmemon/Library/Containers/com.example.flutterFederatedExample/Data/Documents/sample.txt');
-}
+// Future<File> get _localFile async {
+//   final path = await _localPath;
+//   return File('$path/sample.txt');
+// }
 
-Future<File> writeSampleFile(String counter) async {
-  final file = await _localFile;
+// Future<File> writeSampleFile(String counter) async {
+//   final file = await _localFile;
 
-  // Write the file
-  return file.writeAsString('$counter');
-}
+//   // Write the file
+//   return file.writeAsString('$counter');
+// }
