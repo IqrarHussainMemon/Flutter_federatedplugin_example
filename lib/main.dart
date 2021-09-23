bbdd84e3e7632a24f08153c989e92a4931facd6d
IqrarHussainMemon <imemon760@gmail.com>
2021-09-23 15:03:02 +0500

    File Created Successfull in documnet wih direct code without packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:filecreation/filecreation.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,17 +47,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      ;
-    });
+    writeSampleFile("testing");
+    //print(str);
   }
 
   @override
@@ -106,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.blue),
                 ),
-                onPressed: () {},
+                onPressed: _incrementCounter,
                 child: Text("Save"))
           ],
         ),
---
+++ packages/filecreation/lib/filecreation.dart
@@ -0,0 +1,24 @@
+library filecreation;
+
+// import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+
+// Future<String> get _localPath async {
+//   final directory = (await getApplicationDocumentsDirectory());
+//   print(directory.path);
+//   return directory.path;
+// }
+
+Future<File> get _localFile async {
+  //final path = await _localPath;
+  return File(
+      '/Users/iqmemon/Library/Containers/com.example.flutterFederatedExample/Data/Documents/sample.txt');
+}
+
+Future<File> writeSampleFile(String counter) async {
+  final file = await _localFile;
+
+  // Write the file
+  return file.writeAsString('$counter');
+}
